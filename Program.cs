adec1d48f913ea60944e36d646b565ffad4acf2d
JongAAAAAAAAA <90388417+JongAAAAAAAAA@users.noreply.github.com>
2021-10-04 20:56:06 +0900

    기본 수학1_분수 찾기_실패

--- Program.cs
+++ Program.cs
@@ -1,34 +1,54 @@
 ﻿using System;
 using static System.Console;
 
-namespace BAEKJOON_11720
+namespace BAEKJOON_1193
 {
-    class MainApp
+    class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // 첫번째 입력 숫자의 개수 N
-            int inputNum1 = int.Parse(ReadLine());
-            // 두번째 입력 숫자 N개
-            string inputNum2 = ReadLine();
+            int[] arr1 = 0;
+            int[] arr2 = 0;
 
-            // 첫번째 개수만큼 배열크기를 지정
-            int[] intArr = new int[inputNum1];
-            // 두번째 입력 숫자만큼 배열 크기를 지정
-            string[] strArr = new string[inputNum2.Length];
 
-            // 합을 담을 변수
-            int sum = 0;
-            // for을 돌려서 입력된 N개를 배열로 쪼갠다
-            for (int i = 0; i < strArr.Length; i++)
+
+            for (int i = 1; i < num; i++)
             {
-                // strArr배열에 입력받은 값을 하나하나 쪼개서 넣어준다.
-                strArr[i] = Convert.ToString(inputNum2[i]);
-                // sum에 쪼개어진 값을 더해준다.
-                sum += int.Parse(strArr[i]);
+                for (int j = i; j <= i; j--)
+                {
+                    if (j >= 1)
+                    {
+                        for (int a = 0; a < num; a++)
+                        {
+                            arr1[a] = j;
+                        }
+                    }
+
+                }
+                return arr1[num-1];
             }
-            // 더해진 값 출력
-            WriteLine(sum);
+;
+
+            
+
+                for (int k = 1; k < num; k++)
+                {
+                    for (int l = 1; l < k + 1; l++)
+                    {
+                        for (int b = 0; b < num; b++)
+                        {
+                            arr2[b] = l;
+                        }
+                    }
+                    return arr2[num-1];
+                }
+
+                
+
+            int num = int.Parse(ReadLine());
+
+            Console.WriteLine($"(arr1[num-1])/(arr2[num-1])");
+            
         }
     }
 }
